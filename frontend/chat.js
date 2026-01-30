c7a79431b38bbe023b5f1f3f0085508124f00c19
KrishnaChaitanya20 <krishnachaitanyadevunoori@gmail.com>
2026-01-30 20:20:43 +0530

    feat: add leave chat functionality and enhance message display styles

--- frontend/chat.js
+++ frontend/chat.js
@@ -1,36 +1,109 @@
-function getMsg(msg, userId) {
-    return `<div class='msg'><span class='user-id'>${userId} ==></span>${msg}</div>`;
+const BACKEND_URL = "https://api.dkcdev.me";
+
+const chatId = document.getElementById("chatId");
+const userId = document.getElementById("userId");
+const joinBtn = document.getElementById("join");
+const leaveBtn = document.getElementById("leave");
+let eventSource;
+
+// <------------------------ Utility Functions ------------------------>
+function disableJoin() {
+  console.log("inputs disabled");  
+  if (chatId) chatId.disabled = true;
+  if (userId) userId.disabled = true;
+  if (joinBtn) joinBtn.hidden = true;
+  if (leaveBtn) leaveBtn.hidden = false;
 }
+
+function enableJoin() {
+  console.log("inputs enabled");
+  if (chatId) chatId.disabled = false;
+  if (userId) userId.disabled = false;
+  if (joinBtn) joinBtn.hidden = false;
+  if (leaveBtn) leaveBtn.hidden = true;
+}
+
+function getMsg(msg, userId) {
+  return `<div class='msg other'>
+            <div class='user'>${userId}</div>
+            ${msg}
+          </div>`;
+}
+
+function connectToChatServer(chatId, userId) {
+
+  const messagesDiv = document.querySelector(".messages");
+    eventSource = new EventSource(
+    `${BACKEND_URL}/chat/${chatId}?userId=${userId}`,
+  );
+
+  eventSource.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+    if (data.userId === userId) return;
+    if (data.userId === "System") {
+      messagesDiv.innerHTML += `<div class='msg system'> ${data.msg}</div>`;
+    } else {
+      messagesDiv.innerHTML += getMsg(data.msg, data.userId);
+    }
+  };
+
+  eventSource.onerror = (err) => {
+    console.error("SSE error", err);
+    enableJoin();
+    eventSource.close();
+  };
+}
+
+// <------------------------ Chat related Functions ------------------------>
 function joinChat() {
-    const chatId = document.getElementById("chatId").value;
-    const userId = document.getElementById("userId").value;
+  const chatId = document.getElementById("chatId")?.value;
+  const userId = document.getElementById("userId")?.value;
 
-    console.log(chatId,userId);
+  if (!chatId || !userId) {
+    console.error("chatId or userId missing");
+    return;
+  }
 
-    const eventSource = new EventSource(`http://localhost:8080/chat/${chatId}?userId=${userId}`);
-    const messagesDiv = document.querySelector(".messages");
+  const messagesDiv = document.querySelector(".messages");
+  if (!messagesDiv) {
+    console.error(".messages container not found");
+    return;
+  }
 
-    eventSource.onmessage = function(event) {
-        const msg = JSON.parse(event.data).msg;
-        const userID = JSON.parse(event.data).userId;
-        if(userID === userId) return; // skip my own messages
-        messagesDiv.innerHTML += getMsg(msg, userID);
-    };
+  connectToChatServer(chatId, userId);
+  disableJoin();
+}
+
+function leaveChat() {
+  if (eventSource) {
+    eventSource.close();
+    console.log("Disconnected from chat");
+    enableJoin();
+  }
 }
 
 function sendMessage() {
-    const messagesDiv = document.querySelector(".messages");
-    const inputField = document.getElementById("messageInput");
+  const messagesDiv = document.querySelector(".messages");
+  const inputField = document.getElementById("messageInput");
 
-    if(inputField.value.trim() === '') return;
-    //POST "http://localhost:8080/msg/123?userId=123&msg=hi"
-    const chatId = document.getElementById("chatId").value;
-    const userId = document.getElementById("userId").value;
-    fetch(`http://localhost:8080/msg/${chatId}?userId=${userId}&msg=${encodeURIComponent(inputField.value)}`, {
-        method: 'POST'
-    });
-    messagesDiv.innerHTML += `<div class='my-msg'>${inputField.value}</div>`;
-    // messagesDiv.innerHTML += `<div class='msg'>${inputField.value}</div>`;
-    inputField.value = '';
-    
+  if (inputField.value.trim() === "") return;
+  //POST "http://localhost:8080/msg/123?userId=123&msg=hi"
+  const chatId = document.getElementById("chatId").value;
+  const userId = document.getElementById("userId").value;
+  fetch(`${BACKEND_URL}/msg/${chatId}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      userId: userId,
+      msg: inputField.value,
+    }),
+  });
+  messagesDiv.innerHTML += `<div class='msg me'>
+                            <div class='user'>${userId}</div>
+                            ${inputField.value}
+                            </div>`;
+  // messagesDiv.innerHTML += `<div class='msg'>${inputField.value}</div>`;
+  inputField.value = "";
 }
