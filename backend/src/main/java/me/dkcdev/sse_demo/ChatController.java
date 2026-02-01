d63d21cd5be82593dc7a438f53e2f3155336ff65
KrishnaChaitanya20 <krishnachaitanyadevunoori@gmail.com>
2026-02-01 14:19:59 +0530

    refactor: remove unsed imports from ChatController

--- backend/src/main/java/me/dkcdev/sse_demo/ChatController.java
+++ backend/src/main/java/me/dkcdev/sse_demo/ChatController.java
@@ -3,7 +3,6 @@ package me.dkcdev.sse_demo;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import me.dkcdev.sse_demo.models.ChatMsg;
